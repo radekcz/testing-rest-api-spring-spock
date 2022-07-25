90ec954b8bdd5155e9e8fcf14a52fa8fd10f85c2
Radek Krizka <radek.krizka@pricefx.com>
2022-07-25 13:49:43 +0200

    Initial commit

---
+++ src/main/java/cz/rk/spring/TestingSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package cz.rk.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/rk/spring/TestingSpringRestApiApplicationTests.java
@@ -0,0 +1,12 @@
+package cz.rk.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingSpringRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
